680a7744c296f3aad134e7c231c282762ac210eb
DevChattopadhyay <cdev@vmware.com>
2024-03-18 12:59:45 +0530

    Do not penalize Nested Correlated NLJ like nested NLJ

--- src/backend/gporca/libgpdbcost/src/CCostModelGPDB.cpp
+++ src/backend/gporca/libgpdbcost/src/CCostModelGPDB.cpp
@@ -1374,6 +1374,88 @@ CCostModelGPDB::CostNLJoin(CMemoryPool *mp, CExpressionHandle &exprhdl,
 
 	CCost costTotal = CCost(costLocal + costChild);
 
+	COperator::EOperatorId parentOpId = exprhdl.Pop()->Eopid();
+	COperator *popFirstChild = exprhdl.Pop(0);
+	COperator *popSecondChild = exprhdl.Pop(1);
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedInnerNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedInnerNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedInnerNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedLeftOuterNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftOuterNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftOuterNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedLeftSemiNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftSemiNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftSemiNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedInLeftSemiNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedInLeftSemiNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedInLeftSemiNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedLeftAntiSemiNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftAntiSemiNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedLeftAntiSemiNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
+	if (!GPOS_FTRACE(EopttraceEnablePenalizeCorrelatedNLjoin) &&
+		COperator::EopPhysicalCorrelatedNotInLeftAntiSemiNLJoin == parentOpId &&
+		((popFirstChild == nullptr && popSecondChild == nullptr) ||
+		 (popFirstChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedNotInLeftAntiSemiNLJoin ==
+			  popFirstChild->Eopid() &&
+		  popSecondChild != nullptr &&
+		  COperator::EopPhysicalCorrelatedNotInLeftAntiSemiNLJoin !=
+			  popSecondChild->Eopid())))
+	{
+		return costTotal;
+	}
+
 	// amplify NLJ cost based on NLJ factor and stats estimation risk
 	// we don't want to penalize index join compared to nested loop join, so we make sure
 	// that every time index join is penalized, we penalize nested loop join by at least the
